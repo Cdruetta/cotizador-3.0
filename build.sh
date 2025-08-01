a30812131840db05f8d61e037790eeb6e3675ae1
Cdruetta <cristian.e.druetta@gmail.com>
2025-08-01 15:08:35 -0300

    moco

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-# Instalar dependencias
-pip install --upgrade pip
+pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
 
-# Aplicar migraciones
 python manage.py migrate --noinput
-
-# Recolectar archivos estáticos
 python manage.py collectstatic --noinput
