6a836955f2b9db60583b8684d2b132eb3d4d5834
Cdruetta <cristian.e.druetta@gmail.com>
2025-08-15 12:08:29 -0300

    vuelta atras

--- backend/cotizaciones/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CotizacionesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cotizaciones'
-    verbose_name = 'Sistema de Cotizaciones'
