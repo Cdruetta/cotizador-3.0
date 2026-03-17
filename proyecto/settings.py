e3bf70869f89a7bc77ed5b857077f63a53738cd3
Cdruetta <cristian.e.druetta@gmail.com>
2026-03-17 07:51:54 -0300

    .

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get(
     "django-insecure-change-this-in-production"
 )
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
