75d7d77fd759e843e2ca50ed70563a4d2507e0d4
Cristian Eduardo Druetta <cristian.e.druetta@gmail.com>
2026-07-22 08:25:17 -0300

    fix: add --fake-initial to build.sh migrate command

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ pip install --upgrade pip setuptools wheel
 pip install -r backend/requirements.txt
 
 # 🗄 Ejecuta migraciones en la base de datos configurada (PostgreSQL en Render)
-python manage.py migrate --noinput
+python manage.py migrate --noinput --fake-initial
 
 # 🌐 Recolecta archivos estáticos
 python manage.py collectstatic --clear --noinput
